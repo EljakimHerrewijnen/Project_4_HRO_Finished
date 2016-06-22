e36e387e9762cec0fbb70bc3460c9ee000420c48
EljakimHerrewijnen <e.herrewijnen@gmail.com>
2016-06-22 16:34:46 +0200

    Parser aangepast

--- Parser/Parser_Project_4/Parser_Project_4/Fietsendiefstal.cs
+++ Parser/Parser_Project_4/Parser_Project_4/Fietsendiefstal.cs
@@ -32,7 +32,32 @@ namespace Parser_Project_4
             Excel.Range range = sheet.UsedRange;
             int Cell_count = sheet.Cells.Count;
             int rows_count = range.Rows.Count;
-            string tables = (char)34 + "Voorval nummer" + (char)34 + " ," +  (char)34 + "Kennisname" + (char)34 + " ," + (char)34 + "MK" + (char)34 + " ," + (char)34 + "MK omschrijving" + (char)34 + " ," + (char)34 + "Poging" + (char)34 + " ," + (char)34 + "District" + (char)34 + " ," + (char)34 + "Werkgebied" + (char)34 + " ," + (char)34 + "Plaats" + (char)34 + " ," + (char)34 + "Buurt" + (char)34 + " ," + (char)34 + "Straat" + (char)34 + " ," + (char)34 + "Begin dagsoort" + (char)34 + " ," + (char)34 + "Begindatum" + (char)34 + " ," + (char)34 + "Begintijd" + (char)34 + " ," + (char)34 + "Eind dagsoort" + (char)34 + " ," + (char)34 + "Einddatum" + (char)34 + " ," + (char)34 + "Eindtijd" + (char)34 + " ," + (char)34 + "Gemiddelde jaar" + (char)34 + " ," + (char)34 + "Gemiddelde maand" + (char)34 + " ," + "Gemiddelde dagsoort" + (char)34 + "," + (char)34 + "Gemiddelde dagdeel (6 uren)" + (char)34 + "," + (char)34 + "Trefwoord" + (char)34 + "," + (char)34 + "object" + (char)34 +  "," + (char)34 + "merk" + (char)34 + "," + (char)34 + "type" + (char)34 +  "," + (char)34 + "kleur" + (char)34 ;
+            string tables = (char)34 + "Voorval nummer" + (char)34 + " ," 
+                +  (char)34 + "Kennisname" + (char)34 + " ," 
+                + (char)34 + "MK" + (char)34 + " ," 
+                + (char)34 + "MK omschrijving" + (char)34 + " ," 
+                + (char)34 + "Poging" + (char)34 + " ," 
+                + (char)34 + "District" + (char)34 + " ," 
+                + (char)34 + "Werkgebied" + (char)34 + " ," 
+                + (char)34 + "Plaats" + (char)34 + " ," 
+                + (char)34 + "Buurt" + (char)34 + " ," 
+                + (char)34 + "Straat" + (char)34 + " ," 
+                + (char)34 + "Begin dagsoort" + (char)34 + " ," 
+                + (char)34 + "Begindatum" + (char)34 + " ," 
+                + (char)34 + "Begintijd" + (char)34 + " ," 
+                + (char)34 + "Eind dagsoort" + (char)34 + " ," 
+                + (char)34 + "Einddatum" + (char)34 + " ," 
+                + (char)34 + "Eindtijd" + (char)34 + " ," 
+                + (char)34 + "Gemiddelde jaar" + (char)34 + " ," 
+                + (char)34 + "Gemiddelde maand" + (char)34 + " ," 
+                + (char)34 + "Gemiddelde dagsoort" + (char)34 + "," 
+                + (char)34 + "Gemiddelde dagdeel (6 uren)" + (char)34 + "," 
+                + (char)34 + "Trefwoord" + (char)34 + "," 
+                + (char)34 + "object" + (char)34 +  "," 
+                + (char)34 + "merk" + (char)34 + "," 
+                + (char)34 + "type" + (char)34 +  "," 
+                + (char)34 + "kleur" + (char)34 ;
+
             string query = "Create Table if not exists fietsendiefstal("
                 + (char)34 + "Voorval nummer" + (char)34 + "varchar(256) ," 
                 + (char)34 + "Kennisname" + (char)34 + "varchar(256) ," 
@@ -60,19 +85,23 @@ namespace Parser_Project_4
                 + (char)34 + "type" + (char)34 + " varchar(256)," 
                 + (char)34 + "kleur" + (char)34 +" varchar(256))";
 
-            for(int i = 1; i < 20; i++)
+            for(int i = 1; i < 4; i++)
             {
-                query += "INSERT INTO fietsendiefstal(" + tables + ")  VALUES (";
+                if (i != 1) { query += "\n INSERT INTO fietsendiefstal(" + tables + ")  VALUES ("; }
                 for(int j = 1; j <= 25; j++)
                 {
-                    query += (char)34;
-                    query += sheet.Cells[i, j].value;
-                    query += (char)34 + " ,";  
-                    if(j == 7) { query = query.Remove(query.Length - 20); query += ") \n\n\n"; }
+                    string Cellvalues = sheet.Cells[i, j].value;
+                    if (i == 1) { break; }
+                    if(j == 13) { query += "\n"; }
+                    query += (char)39;
+                    query += Cellvalues;
+                    query += (char)39 + " ,";  
+                    if(j == 7) { query = query.Remove(query.Length - 20); query += ") \n"; }
                 }
                 Console.WriteLine("Parsed one line..." + i);
             }
             Form1._Form1.Query = query;
+            workbook.Close();
             Console.WriteLine(query);
         }
 
