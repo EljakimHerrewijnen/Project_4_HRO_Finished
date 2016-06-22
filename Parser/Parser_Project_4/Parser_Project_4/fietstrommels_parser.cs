b00d337bb5a13d5a112f79eb51882c5749cdf950
Joostdw1 <joostbenik@hotmail.com>
2016-06-22 09:23:50 +0200

    hi

--- Parser/Parser_Project_4/Parser_Project_4/fietstrommels_parser.cs
+++ Parser/Parser_Project_4/Parser_Project_4/fietstrommels_parser.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Parser_Project_4
 {
     class fietstrommels_parser
     {
+        Excel.Application xlApp = new
+        Microsoft.Office.Interop.Excel.Application();
+
 
 
     }
