eb77184a8353af01b1c5a8266c50e62d1e6e06a0
Jan-Paul <jpmosterdijk@gmail.com>
2016-06-23 11:20:49 +0200

    voor timo

---
+++ Code/Chart generator Jan-Paul/lolol/app/src/main/java/com/example/jan_paul/lolol/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.jan_paul.lolol;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ExpandableListView;
+import android.widget.ListView;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayAdapter<String> adapter;
+    ArrayList<String> itemList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] items={"Pie","Line","Etc"};
+        itemList=new ArrayList<String>(Arrays.asList(items));
+        adapter=new ArrayAdapter<String>(this,R.layout.list_item,R.id.checkbox,itemList);
+        ExpandableListView listV=(ExpandableListView) findViewById(R.id.listView);
+        listV.setAdapter(adapter);
+    }
+
+    public void onClick(View v){
+        startActivity(new Intent(MainActivity.this, Chart.class));
+
+
+
+    }
+
+
+}
