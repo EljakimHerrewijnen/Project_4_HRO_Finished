4a8d9c92da8641a575a6f0532c9f11cc4842e223
FireStormHR <bret_hart_hitman_raw@hotmail.com>
2016-06-27 11:26:29 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Code/Chart generator Jan-Paul/lolol/.idea/workspace.xml

---
+++ Code/Chart generator Jan-Paul/lolol/app/src/main/java/Design_Patterns/None.java
@@ -0,0 +1,17 @@
+package Design_Patterns;
+
+/**
+ * Created by Timo on 24/06/2016.
+ */
+public class None<T> implements Option<T> {
+
+    @Override // I did auto resolve: add exception to method signature
+    public <U extends Option<T>> U Visit(IOptionVisitor<T, U> visitor) throws Exception {
+        return visitor.onNone();
+    }
+
+    @Override
+    public boolean IsSome() {
+        return false;
+    }
+}
