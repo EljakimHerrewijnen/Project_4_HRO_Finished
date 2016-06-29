b998c6d2580a2bcbc031c4e090430e947441fc14
Joostdw1 <joostbenik@hotmail.com>
2016-06-29 10:49:49 +0200

    database werkt!

--- Code/Chart generator Jan-Paul/lolol/app/src/main/java/com/example/jan_paul/lolol/MainActivity.java
+++ Code/Chart generator Jan-Paul/lolol/app/src/main/java/com/example/jan_paul/lolol/MainActivity.java
@@ -90,7 +90,7 @@ public class MainActivity extends AppCompatActivity {
         btnLoadData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View vw) {
-                DatabaseAccess databaseAccess = DatabaseAccess.getInstance(content);
+                DatabaseAccess databaseAccess = DatabaseAccess.getInstance(getApplicationContext());
                 databaseAccess.open();
                 String deelgemeente = databaseAccess.getDeelgemeente2();
                 databaseAccess.close();
