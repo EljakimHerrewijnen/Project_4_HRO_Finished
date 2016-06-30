28ba3e812698f66d98d13c765cff4648452df367
FireStormHR <bret_hart_hitman_raw@hotmail.com>
2016-06-30 09:55:40 +0200

    GPS IS ADDED!! KEEP IT

--- Code/Chart generator Jan-Paul/lolol/app/src/main/java/com/example/jan_paul/lolol/MainActivity.java
+++ Code/Chart generator Jan-Paul/lolol/app/src/main/java/com/example/jan_paul/lolol/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.jan_paul.lolol;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.sqlite.SQLiteDatabase;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,18 +22,25 @@ import android.database.sqlite.*;
 import android.content.Context;
 
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener  {
     ArrayAdapter<String> adapter;
     ArrayList<String> itemList;
     public boolean pie;
     public boolean line;
     Context content;
+    public GoogleApiClient mGoogleApiClient = null;
+    public Location mLastLocation = null;
 
-    Button btnLoadData;
+    Button btnLoadData, mLatitudeText, mLongitudeText;
     EditText editTxtData;
     ExpandableListView expandableListView;
     ExpandableListAdapter expandableListAdapter;
@@ -42,6 +53,9 @@ public class MainActivity extends AppCompatActivity {
 
         btnLoadData =(Button)findViewById(R.id.btn_database);
         editTxtData = (EditText)findViewById(R.id.editTextData);
+        mLatitudeText = (Button) findViewById(R.id.buttonworldy);
+        mLongitudeText = (Button) findViewById(R.id.buttonworldx);
+
         expandableListView = (ExpandableListView) findViewById(R.id.expandableListView);
         expandableListDetail = ExpandableListDataPump.getData();
         expandableListTitle = new ArrayList<String>(expandableListDetail.keySet());
@@ -97,7 +111,27 @@ public class MainActivity extends AppCompatActivity {
                 editTxtData.setText("dit moet weg");
             }
         });
+
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks((GoogleApiClient.ConnectionCallbacks) this)
+                    .addOnConnectionFailedListener((GoogleApiClient.OnConnectionFailedListener) this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
     }
+
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+
 
     //gets called when something is toggled in main activity
     public void onToggle(View v){
@@ -120,4 +154,33 @@ public class MainActivity extends AppCompatActivity {
         startActivity(new Intent(MainActivity.this, ChartActivity.class));
     }
 
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null) {
+            mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
+            mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 }
