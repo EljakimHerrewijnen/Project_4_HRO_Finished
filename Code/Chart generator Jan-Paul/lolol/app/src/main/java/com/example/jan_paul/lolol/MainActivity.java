8bbb09ac4b85344b6e57b564b47bd39fd943f00d
Joostdw1 <joostbenik@hotmail.com>
2016-06-30 11:08:20 +0200

    asd

--- Code/Chart generator Jan-Paul/lolol/app/src/main/java/com/example/jan_paul/lolol/MainActivity.java
+++ Code/Chart generator Jan-Paul/lolol/app/src/main/java/com/example/jan_paul/lolol/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.jan_paul.lolol;
 
+
+import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
 import android.widget.Switch;
+import android.widget.Toast;
+import android.database.sqlite.*;
+import android.content.Context;
+
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -16,13 +25,12 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     ArrayAdapter<String> adapter;
     ArrayList<String> itemList;
+    public boolean pie;
+    public boolean line;
+    Context content;
 
-    //filters
-    public static String ChartType = "";
-
-    public String FilePath = "/data";
-    public String FileName = "Fietsendiefstal";
-
+    Button btnLoadData;
+    EditText editTxtData;
     ExpandableListView expandableListView;
     ExpandableListAdapter expandableListAdapter;
     List<String> expandableListTitle;
@@ -31,38 +39,85 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstance){
         super.onCreate(savedInstance);
         setContentView(R.layout.activity_main);
+
+        btnLoadData =(Button)findViewById(R.id.btn_database);
+        editTxtData = (EditText)findViewById(R.id.editTextData);
         expandableListView = (ExpandableListView) findViewById(R.id.expandableListView);
         expandableListDetail = ExpandableListDataPump.getData();
         expandableListTitle = new ArrayList<String>(expandableListDetail.keySet());
         expandableListAdapter = new com.example.jan_paul.lolol.CustomExpandableListAdapter(this, expandableListTitle, expandableListDetail);
         expandableListView.setAdapter(expandableListAdapter);
+
+        expandableListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+              //  Toast.makeText(getApplicationContext(),
+                //        expandableListTitle.get(groupPosition) + " List Expanded."
+                 //       Toast.LENGTH_SHORT).show();
+                //Above is for displaying text.
+            }
+        });
+
+        expandableListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+                //Toast.makeText(getApplicationContext(),
+                  //      expandableListTitle.get(groupPosition) + " List Collapsed."
+                    //    Toast.LENGTH_SHORT).show();
+                //Above is for displaying text
+
+            }
+        });
+
+        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                Toast.makeText(
+                        getApplicationContext(),
+                        expandableListTitle.get(groupPosition)
+                                + " -> "
+                                + expandableListDetail.get(
+                                expandableListTitle.get(groupPosition)).get(
+                                childPosition), Toast.LENGTH_SHORT
+                ).show();
+                return false;
+            }
+        });
+
+        btnLoadData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View vw) {
+                DatabaseAccess databaseAccess = DatabaseAccess.getInstance(getApplicationContext());
+                databaseAccess.open();
+                List<Data> deelgemeente = databaseAccess.getMostfietstrommels();
+                databaseAccess.close();
+                editTxtData.setText("dit moet weg");
+            }
+        });
     }
+
+    //gets called when something is toggled in main activity
+    public void onToggle(View v){
+        //checks what is toggled, and gets its boollean value.
+        Switch s = (Switch) v;
+        if (s.isChecked() == true){
+            //if(s.getId() == 2){
+
+            //}
+        }
+        else{
+
+        }
+
+    }
+
 
     //opens chart activity
     public void onGenerate(View v){
-        /*
-        getSupportFragmentManager()
-                .beginTransaction()
-                .add(android.R.id.content, ChartFragment.newInstance(), "rageComicList")
-                .commit();
-                */
-        Intent intent = new Intent(this, ChartActivity.class);
-        startActivity(intent);
+        startActivity(new Intent(MainActivity.this, ChartActivity.class));
     }
-
-    public void onToggle(View v){
-        Switch s = (Switch) v;
-        setFilter(s.getText().toString(), s.isChecked());
-    }
-
-    public void setFilter(String s, boolean b){
-        if (s == "Pie chart" && b){ChartType = "pie";}
-        if (s == "Pie chart" && !b){ChartType = "";}
-        if (s == "Line chart" && b){ChartType = "line";}
-        if (s == "Line chart" && !b){ChartType = "";}
-    }
-
-
-
 
 }
